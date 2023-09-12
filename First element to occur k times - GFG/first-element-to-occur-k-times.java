019b5066dffe8c69844d1431a1142c03c6fb1914
bhagchand-kakade <102870967+bhagchand-kakade@users.noreply.github.com>
2023-09-12 17:54:12 +0530

    Added solution - LeetHub

---
+++ First element to occur k times - GFG/first-element-to-occur-k-times.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(arr, n, m));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int[] a, int n, int k) { 
+        
+        /*for(int i=0;i<n;i++){
+            int count =0;
+            for(int j=0;j<n;j++){
+                if(a[i]==a[j]){
+                    count++;
+                }
+            }
+            if(count==k){
+                return a[i];
+            }
+        }
+        return -1;*/
+        
+        Map<Integer,Integer> map=new LinkedHashMap<>();
+        if(k==1){
+            return a[0];
+        }
+        for(int i=0;i<n;i++){
+            if(map.containsKey(a[i])){
+                int v=map.get(a[i])+1;
+                if(v>=k){
+                    return a[i];
+                }else{
+                map.put(a[i],v);
+                }
+            }
+            else{
+                map.put(a[i],1);
+            }
+        }
+        
+        return -1;
+    } 
+}
